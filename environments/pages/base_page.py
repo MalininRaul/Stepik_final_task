1c0486e9cf63b34456fc0730e8216f30de75f3ff
MalininRaul <raulmalinin83@gmail.com>
2022-11-24 23:44:05 +0600

    оптимизация теста

--- environments/pages/base_page.py
+++ environments/pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException
 # добавим конструктор — метод, который вызывается, когда мы создаем объект.
 # Конструктор объявляется ключевым словом __init__. В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
 # Внутри конструктора сохраняем эти данные как аттрибуты нашего класса. Получается примерно так:
@@ -11,3 +12,18 @@ class BasePage():
 # используя метод get()
     def open(self):
         self.browser.get(self.url)
+
+    # напишем вспомогательный метод поиска элемента, который будет возвращать нам True или False
+    def __init(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    # метод is_element_present, в котором будем перехватывать исключение. В него будем передавать два аргумента: как искать (css, id, xpath и тд)
+    # и собственно что искать (строку-селектор)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- environments/pages/main_page.py
+++ environments/pages/main_page.py
@@ -8,3 +8,7 @@ class MainPage(BasePage):
     def go_to_login_page(self): #В аргументы больше не надо передавать экземпляр браузера, мы его передаем и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент self , чтобы иметь доступ к атрибутам и методам класс
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    #Проверка, есть ли ссылка на логин на странице
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented" #намеренно сделали селектор неправильным, чтобы посмотреть, что именно выдаст тест, если поймает баг
